0807559936a1791a2f4b0d8d50b431ab015809dd
sajad mirjalili <sajadmrjll@gmail.com>
2024-10-29 16:08:19 +0330

    [ADD] router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
-import { ThemeProvider } from "@emotion/react";
-import "./App.css";
+import { ThemeProvider } from "@emotion/react"; 
 import theme from "./theme/theme";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+  },
+]);
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <></>
+      <RouterProvider router={router} />
     </ThemeProvider>
   );
 }
